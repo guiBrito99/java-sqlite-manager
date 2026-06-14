e7db3fa0fd0dc3abf65724012b08af92a0590ab5
gsgdb <gsgdb@Lenovo>
2026-06-14 11:51:54 -0300

    * No longer accepts null columns in table creation

--- src/main/java/ab/db/SQLManager.java
+++ src/main/java/ab/db/SQLManager.java
@@ -99,22 +99,17 @@ public class SQLManager {
 
 		String command;
 		do {
-			System.out.print("Table columns, separated by comma:");
+			System.out.println("Table columns, separated by comma:");
 
 			command = this.scanner.nextLine();
 
-			// This ensures the split command not passing a blank String[]
-			if (!command.isBlank())
+			if (command != "") {
 				columns = command.split(",");
+				System.out.println("Columns: " + Arrays.toString(columns));
+				System.out.println("Confirm y/n");
+			}
 
-			System.out.println("Table columns: " + (columns != null ? Arrays.toString(columns) : ""));
-
-			/*
-			 * To avoid confusion(columns array empty, returning directly to the beginning
-			 * of the loop), only prints the confirm prompt if columns is valid
-			 */
-			System.out.print("Confirm y/n\n");
-		} while (this.scanner.nextLine().charAt(0) != 'y');
+		} while (command == "" || this.scanner.nextLine().charAt(0) != 'y');
 
 		// Adding the table to the data base
 		String sql = "CREATE TABLE IF NOT EXISTS " + tableName;
@@ -129,13 +124,11 @@ public class SQLManager {
 
 		this.runSQL(sql);
 
-		String[] allColumns = new String[1 + (columns == null ? 0 : columns.length)];
+		String[] allColumns = new String[columns.length + 1];
+
 		allColumns[0] = "id";
-
-		if (columns != null)
-			for (int i = 0; i < columns.length; i++)
-				allColumns[i + 1] = columns[i];
-
+		for (int i = 0; i < columns.length; i++)
+			allColumns[i + 1] = columns[i];
 		// Adding the desired table to the ArrayList representation of tables
 		this.tables.add(new Table(tableName, allColumns));
 	}
