27a2309f0598cd4170942f10b336284ee7ba495c
gsgdb <gsgdb@200.235.91.16>
2026-06-17 16:23:49 -0300

    * View class now holds a DatabaseController * The switch function now call each DatabaseController function depending on user input

--- src/main/java/ab/db/View.java
+++ src/main/java/ab/db/View.java
@@ -8,16 +8,18 @@ public class View {
 
 	public static void main(String... args) throws SQLException {
 
+		DatabaseController databaseController = new DatabaseController();
+
 		if (args.length == 0) {
 			int command;
 
 			do {
 				System.out.println("Select the command:");
 				System.out.println("1 - Print");
-				System.out.println("2 - Add table");
-				System.out.println("3 - Remove table");
+				System.out.println("2 - Create table");
+				System.out.println("3 - Delete table");
 				System.out.println("4 - Insert row");
-				System.out.println("5 - Remove row");
+				System.out.println("5 - Delete row");
 				System.out.println("6 - Update row");
 				System.out.println("0 - Exit");
 
@@ -31,16 +33,22 @@ public class View {
 
 				switch (command) {
 				case 1:
+					databaseController.print();
 					break;
 				case 2:
+					databaseController.createTable();
 					break;
 				case 3:
+					databaseController.deleteTable();
 					break;
 				case 4:
+					databaseController.insertRow();
 					break;
 				case 5:
+					databaseController.deleteRow();
 					break;
 				case 6:
+					databaseController.updateRow();
 					break;
 				default:
 				}
