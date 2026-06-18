1647436f9bc03945db61e94d24523c91341b622f
gsgdb <gsgdb@200.235.91.16>
2026-06-18 10:27:57 -0300

    * Modifying the main class that when the "--print" argument is passed, the tables of the database are printed

--- src/main/java/ab/db/View.java
+++ src/main/java/ab/db/View.java
@@ -1,61 +1,77 @@
 package ab.db;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class View {
 	static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String... args) throws SQLException {
-
 		DatabaseController databaseController = new DatabaseController();
-
 		if (args.length == 0) {
-			int command;
+			databaseMenu(databaseController);
+		} else {
+			ArrayList<String> arguments = new ArrayList<>(List.of(args));
 
-			do {
-				System.out.println("Select the command:");
-				System.out.println("1 - Print");
-				System.out.println("2 - Create table");
-				System.out.println("3 - Delete table");
-				System.out.println("4 - Insert row");
-				System.out.println("5 - Delete row");
-				System.out.println("6 - Update row");
-				System.out.println("0 - Exit");
-
-				try {
-					command = View.scanner.nextInt();
-				} catch (Exception e) {
-					System.out.println("Type in valid integer");
-					View.scanner.next();
-					command = -1;
-				}
-
-				switch (command) {
-				case 1:
+			for (String argument : arguments) {
+				switch (argument.toLowerCase()) {
+				case "--print":
 					databaseController.print();
-					break;
-				case 2:
-					databaseController.createTable();
-					break;
-				case 3:
-					databaseController.deleteTable();
-					break;
-				case 4:
-					databaseController.insertRow();
-					break;
-				case 5:
-					databaseController.deleteRow();
-					break;
-				case 6:
-					databaseController.updateRow();
 					break;
 				default:
 				}
-
-			} while (command != 0);
-
+			}
 		}
+
+	}
+
+	private static void databaseMenu(DatabaseController databaseController) throws SQLException {
+
+		int command;
+
+		do {
+			System.out.println("Select the command:");
+			System.out.println("1 - Print");
+			System.out.println("2 - Create table");
+			System.out.println("3 - Delete table");
+			System.out.println("4 - Insert row");
+			System.out.println("5 - Delete row");
+			System.out.println("6 - Update row");
+			System.out.println("0 - Exit");
+
+			try {
+				command = View.scanner.nextInt();
+			} catch (Exception e) {
+				System.out.println("Type in valid integer");
+				View.scanner.next();
+				command = -1;
+			}
+
+			switch (command) {
+			case 1:
+				databaseController.print();
+				break;
+			case 2:
+				databaseController.createTable();
+				break;
+			case 3:
+				databaseController.deleteTable();
+				break;
+			case 4:
+				databaseController.insertRow();
+				break;
+			case 5:
+				databaseController.deleteRow();
+				break;
+			case 6:
+				databaseController.updateRow();
+				break;
+			default:
+			}
+
+		} while (command != 0);
 
 		System.out.println("Exiting...");
 	}
